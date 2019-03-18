4ccfd0ff790eecc5a0924f3e594eb2c07455af7d
Yorbi Cardozo <yorbicardozo2@gmail.com>
2019-03-18 09:42:56 -0500

    Commit inicial

---
+++ app.js
@@ -0,0 +1,19 @@
+const argv = require('./config/yargs').argv;
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite)
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: ${ archivo.green}`))
+            .catch(e => console.log(e));
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+}
